513f70d51a0cc67d340c3c944bf887eea01a6caf
Francesc F <francesc.forn_2@hotmail.com>
2024-04-13 10:54:22 +0200

    map loader

---
+++ src/grid_loader.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "raylib.h"
+#include "grid.h"
+
+Grid loadGrid(char *filename) {
+    Image image = LoadImage(filename);
+    Color *pixels = LoadImageColors(image);
+    unsigned int width = image.width;
+    unsigned int height = image.height;
+
+    Grid grid = CreateGrid(width, height);
+
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            Color pixel = pixels[j * width + i];
+
+            if (pixel.b > 0)
+                SetGridTile(&grid, i, j, PATH_TILE);
+        }
+    }
+
+    return grid;
+}
---
+++ src/grid_loader.h
@@ -0,0 +1,7 @@
+#ifndef GRID_LOADER_H_
+#define GRID_LOADER_H_
+
+include "grid.h"
+
+Grid loadGrid(char* filename);
+
